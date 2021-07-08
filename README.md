2e2229409b58f297e59c9f4adb96cf05e257af89
Xheldon <c1006044256@gmail.com>
2021-07-08 12:11:12 +0800

    添加代理、设置页数、页码等搜索选项

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-change the default language to chinese
+## 说明
 
-Note: some api not show in result,like: Math.acosh: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math/acosh but english have.
+1. 使用 MDN 搜索 API 自带的 popularity 字段对结果进行排序，即按热度进行排序。
+2. 原始访问 MDN 地址非常慢，为了加快速度，在访问 MDN 的时候可以设置本地代理，具体方法见下面 `代理` 一节
+3. 原作者使用 documents[n].highlight.body[0] 作为结果，其优点是内容更详细，缺点是需要 html 过滤，作者只使用了简单的过滤，因此搜索结果会有一些 html 字符和乱码。我改成使用 documents[n].summary 作为搜索结果的详情，优点是没有 html 字符/乱码的干扰，更清晰，缺点是内容较少，只能看第一句，不过即使是长了在 Alfred 中显示的也是省略号，没有意义。
+4. 因为 MDN 的搜索 API 的接口总是变化，因此如果发现不可用，请 [电报联系我](https://t.me/xheldon_saloon) 或者在 Github 给我提 [Issue](https://github.com/Xheldon/alfred-mdn-cn/issues)。
+5. 搜索结果可能不存在，因为中文存在未翻译的情况，如果没有搜索结果，则点击查看全部的时候，显示的是英文。
+6. 结果默认返回 10 条，（包含过滤的条数），不使用代理进行搜索。
+7. 我过滤不显示了一部分内容，包括：
+    1. summary 以 「«」符号开头的的内容，因为其内容没有意义。
 
-修改默认语言为中文，注意一些结果在中文搜索下没有，比如 Math.acosh。
+## 建议
 
-# alfred-mdn
-> Search through the MDN documentation
+直接使用 node fetch 较慢，建议使用代理，代理方法见下
+
+
+# alfred-mdn-cn
+> 搜索 MDN 中文文档
 
 ![screenshot](screenshot.png)
 
@@ -19,13 +29,43 @@ $ npm install -g alfred-mdn-cn
 *Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
 
 
-## Usage
+## 使用
 
-In Alfred, type `mdn`, <kbd>Enter</kbd>, and your query.
+### 常规搜索
 
-Select an item and press <kbd>Enter</kbd> to go to its [MDN](https://developer.mozilla.org/en-US/) documentation.
+在 Alfred 中，输入 `mdn` + 空格 + 搜索内容，如 `mdn filter`。
 
+选择一个 item，回车，即可打开 MDN 官方中文文档查看。
+
+### 使用本地代理加快搜索
+
+如果本地代理地址为 `127.0.0.1:7890`，则在 Alfred 中，输入 `mdn --p=7890` ，然后重新搜索，即可使用代理进行搜索。
+
+注：若代理配置错误，则会出现如下提示：
+
+![proxy error](img/RequestError.jpg)
+
+### 设置每页搜索条数
+
+输入 `mdn --s=20` ，则之后的每次搜索，即可将结果变为 20条（含过滤掉的）
+
+### 设置搜索页数
+
+输入 `mdn <query> 2` ，则可以搜索第二页的内容，如 `mdn sort 2`（注意空格），仅对当次搜索有效。
+
+### 还原设置
+
+如果要恢复默认搜索，则在 Alfred 中，将各个设置参数改成 `dft` 即可还原设置，如 `mdn --p=dft --s=dft`
+
+### 支持同时设置
+
+如 `mdn --p=dft --s=30 2` 则表示不使用代理、搜索结果设置为 30 条，搜索第二页的内容；其中不使用代理和搜索结果设置为 30 条对后续搜索都有效，搜索第二页的内容仅对当前搜索结果生效。
+
+## 注意
+
+如果发现搜索结果出不来，很可能是因为你配置错了，请按照文档重新配置！
 
 ## License
 
 MIT © [Klaas Cuvelier](https://klaascuvelier.io)
+[Xheldon](https://xheldon.com)
--- index.js
+++ index.js
@@ -1,38 +1,145 @@
 "use strict";
 
 const alfy = require("alfy");
+const tunnel = require('tunnel');
 const mdnApiBase = 'https://wiki.developer.mozilla.org/api/v1/search/zh-CN';
-const mdnDocsBase = 'https://developer.mozilla.org/zh-CN/docs/';
+const mdnApiUSBase = 'https://wiki.developer.mozilla.org/api/v1/search/en-US';
+const mdnDocsBase = 'https://developer.mozilla.org';
 
-alfy.fetch(`${mdnApiBase}?q=${alfy.input}`, { transform: ({ documents }) => documents }).then(results => {
-        const items = (results || []).map(result => {
-            const {title, excerpt, slug} = result;
-            const subtitle = stripHtml(excerpt);
-            const url = `${mdnDocsBase}${slug}`;
+let input = alfy.input.trim();
+let page = 1;
+let query = '';
 
-            return {
-                title,
-                subtitle,
-                autoComplete: title,
-                arg: url,
-                quicklookurl: url
+
+function setByPair(pair) {
+    // Note: 不确定 当前版本的 node 是否支持数组解构，先保险写法
+    let key = pair[0];
+    let value = pair[1];
+    if (value === 'dft') {
+        alfy.config.delete(key);
+        // Note: node 版本可能模板字符串不支持
+        alfy.output([{
+            title: `${key} 参数重置成功！`,
+            subtitle: '请重新尝试搜索'
+        }]);
+        return;
+    } else {
+        alfy.config.set(key, value);
+        alfy.output([{
+            title: `${key} 参数设置成功！`,
+            subtitle: '请重新尝试搜索'
+        }]);
+        return;
+    }
+}
+
+let arr = input.split(' ').map(function(i){return i.trim();}).filter(Boolean);
+
+// Note: for 里面 return 无效，这个 alfy 写的不咋地啊，有空改造下
+if (input.includes('--')) {
+    // Note: 存在设置项
+    for (let k in arr) {
+        let item = arr[k].trim();
+        if (item.startsWith('--')) {
+            let pair = item.slice(2).split('=');
+            if (pair.length !== 2) {
+                alfy.output([{
+                    title: 'Error！',
+                    subtitle: '部分或全部参数非法，请参照 README 设置',
+                    quicklookurl: 'https://github.com/Xheldon/alfred-mdn-cn/blob/master/README.md'
+                }]);
+                return;
+            } else {
+                return setByPair(pair);
             }
-        });
+        }
+    }
+    return;
+} else {
+    for (let k in arr) {
+        let item = arr[k].trim();
+        if (k === '0') {
+            query = item;
+        } else if (!Number.isNaN(item)){
+            page = Number(item);
+        }
+    }
+}
 
-        // No results
-        if (items.length === 0 && false) {
-            const url = `${mdnApiBase}?q=${alfy.input}`;
 
-            items.push({
-                title: `显示 '${alfy.input}' 的所有结果...`,
-                arg: url,
-                quicklookurl: url
-            });
+
+let queryUrl = `${mdnApiBase}?q=${query}&size=${alfy.config.get('s') || 10}&page=${page}`
+
+alfy.fetch(queryUrl, Object.assign({
+    transform: ({ documents }) => documents,
+}, alfy.config.get('p') ? {
+    agent: {
+            https: tunnel.httpsOverHttp({
+                proxy: {
+                    host: '127.0.0.1',
+                    port: alfy.config.get('p'),
+                    headers: {
+                        ':authority': 'developer.mozilla.org',
+                        ':method': 'GET',
+                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                        'accept-encoding': 'gzip, deflate, br',
+                        'cache-control': 'no-cache',
+                        ':path': `/api/v1/search/zh-CN?q=${query}&size=${alfy.config.get('s') || 10}&page=${page}`,
+                        ':scheme:': 'https',
+                        'sec-ch-ua': ' Not;A Brand;v="99", "Google Chrome";v="91", "Chromium";v="91"',
+                        'sec-fetch-dest': 'document',
+                        'sec-fetch-mode': 'navigate',
+                        'sec-fetch-site': 'none',
+                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+                    }
+                }
+            })
+        }
+} : {})).then(results => {
+    // Note: 这里不能 console.log 任何内容否则报错
+    const items = (results || []).sort((a, b) => {
+        let c = Number(a.popularity);
+        let d = Number(b.popularity);
+        if (!Number.isNaN(c) && !Number.isNaN(d)) {
+            return d - c;
+        }
+        return 0;
+    }).map(result => {
+        const {title, highlight, summary, mdn_url} = result;
+        if (summary.startsWith('«')) {
+            return false;
+        }
+        // Note: 不从 body 上找，从 summary 上即可了
+        // const excerpt = highlight && highlight.body && highlight.body.length > 0 ? highlight.body[0] : "";
+        // const subtitle = stripHtml(excerpt);
+        let subtitle = summary;
+
+        if (subtitle.startsWith(title)) {
+            subtitle = '该' + subtitle.slice(title.length).trim();
         }
 
-        alfy.output(items);
-    });
+        const url = `${mdnDocsBase}${mdn_url}`;
 
-function stripHtml(text) {
-    return text.replace(/<[^>]+>/g, '');
-}
+        return {
+            title,
+            subtitle,
+            autoComplete: title,
+            arg: url,
+            quicklookurl: url
+        }
+    }).filter(Boolean);
+
+    // No results
+    if (items.length === 0) {
+        const url = `${mdnApiUSBase}?q=${query}`;
+
+        items.push({
+            title: `显示 '${query}' 的所有结果...`,
+            arg: url,
+            quicklookurl: url
+        });
+    }
+
+    alfy.output(items);
+});
+
